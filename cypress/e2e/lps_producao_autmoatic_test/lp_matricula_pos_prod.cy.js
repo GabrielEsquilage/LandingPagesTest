3d855f4be75d5209a5676ef049803618270865e6
Gabriel Esquilage[ <roccaesquil19910@gmail.com>
2026-01-26 09:40:49 -0300

    ajustment prod test cases

--- cypress/e2e/lps_producao_autmoatic_test/lp_matricula_pos_prod.cy.js
+++ cypress/e2e/lps_producao_autmoatic_test/lp_matricula_pos_prod.cy.js
@@ -3,89 +3,9 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
     let sharedEmail;
     let sharedName;
 
-    it('Deve fazer o cadastro completo - cupom 100%', () => {
-
-        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
-
-        cy.generateRandomName().then(nome => {
-            sharedName = nome;
-            cy.logToTerminal(`Nome gerado e salvo: ${nome}`)
-            cy.contains('Nome Completo').scrollIntoView().parent()
-                .find('input, textarea').clear().type(nome)
-        })
-
-        cy.generateValidCpf().then(cpf => {
-            sharedCpf = cpf;
-            cy.logToTerminal(`CPF gerado e salvo: ${cpf}`)
-            cy.contains('CPF').scrollIntoView().parent()
-                .find('input, textarea').clear().type(cpf)
-        })
-
-        cy.contains('Telefone').scrollIntoView().parent()
-            .find('input, textarea').clear().type('44444444444')
-
-        cy.generateRandomName().then(nome => {
-            cy.contains('Nome Social').scrollIntoView().parent()
-                .find('input, textarea').clear().type(nome)
-        })
-
-        cy.contains('Data de Nascimento ').scrollIntoView().parent()
-            .find('input').clear().type('2000-01-01')
-
-        cy.generateEmail().then(email => {
-            sharedEmail = email;
-            cy.logToTerminal(`Email gerado e salvo: ${email}`)
-            cy.contains('E-mail').scrollIntoView().parent()
-                .find('input, textarea').clear().type(email)
-        })
-
-        cy.contains('button', 'Próximo').scrollIntoView().click()
-
-        cy.contains('Digite seu CEP').scrollIntoView().parent()
-            .find('input, textarea').clear().type('87020-015')
-
-        cy.wait(1000)
-
-        cy.contains('Número').scrollIntoView().parent()
-            .find('input, textarea').clear().type('000')
-
-        cy.contains('Button', 'Próximo').scrollIntoView().click()
-
-        cy.get('button[role="combobox"]')
-            .contains('Selecione um curso')
-            .click();
-
-        cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-            .should('be.visible')
-            .click();
-
-        cy.selecionarOpcaoAleatoriaDireto('Estado')
-        cy.selecionarOpcaoAleatoriaDireto('Cidade')
-        cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
-        cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
-        cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
-
-        cy.contains('label', 'Pix').click()
-
-        cy.contains('Possui cupom de desconto?').scrollIntoView().parent()
-            .find('input, textarea').clear().type('bolsa100')
-
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
-
-        cy.contains('p', 'Pagamento isento com cupom de 100% de desconto!')
-            .should('be.visible')
-
-        cy.contains('Button', 'Enviar').scrollIntoView().click()
-
-        cy.contains('button', 'Fechar', { timeout: 60000 })
-            .should('be.visible')
-            .and('be.enabled')
-    })
-
     it('Deve fazer o cadastro completo - Sem Cupom - PIX', () => {
 
-        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
+        cy.visit('https://matriculapos.fatecie.edu.br/inscription')
 
         cy.generateRandomName().then(nome => {
             sharedName = nome;
@@ -135,7 +55,7 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
             .contains('Selecione um curso')
             .click();
 
-        cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
+        cy.contains('ESPECIALIZAÇÃO EM ALFABETIZAÇÃO E LETRAMENTO')
             .should('be.visible')
             .click();
 
@@ -152,14 +72,16 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
 
         cy.contains('Button', 'Enviar').scrollIntoView().click()
 
-        cy.contains('button', 'Fechar', { timeout: 60000 })
-            .should('be.visible')
-            .and('be.enabled')
+        cy.contains('div', 'Informações de Pagamento')
+        cy.get('img[alt="Imagem do meio de pagamento"]')
+            .should('have.attr', 'src')
+            .and('include', 'payment_method=pix');
+
     })
 
-    it('Deve fazer o cadastro completo - Sem Cupom - cartão de crédito', () => {
+        it('Deve fazer o cadastro completo - Sem Cupom - BOLETO', () => {
 
-        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
+        cy.visit('https://matriculapos.fatecie.edu.br/inscription')
 
         cy.generateRandomName().then(nome => {
             sharedName = nome;
@@ -209,7 +131,80 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
             .contains('Selecione um curso')
             .click();
 
-        cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
+        cy.contains('ESPECIALIZAÇÃO EM ALFABETIZAÇÃO E LETRAMENTO')
+            .should('be.visible')
+            .click();
+
+        cy.selecionarOpcaoAleatoriaDireto('Estado')
+        cy.selecionarOpcaoAleatoriaDireto('Cidade')
+        cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
+        cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
+        cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
+
+        cy.contains('label', 'Boleto').click()
+
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
+
+        cy.contains('Button', 'Enviar').scrollIntoView().click()
+
+        cy.contains('div', 'Informações de Pagamento')
+        cy.contains('div', 'Código do Boleto')
+    })
+
+    it('Deve fazer o cadastro completo - Sem Cupom - cartão de crédito - Deve Retornar Erro no Pagamento com Cartão de Teste', () => {
+
+        cy.visit('https://matriculapos.fatecie.edu.br/inscription')
+
+        cy.generateRandomName().then(nome => {
+            sharedName = nome;
+            cy.logToTerminal(`Nome gerado e salvo: ${nome}`)
+            cy.contains('Nome Completo').scrollIntoView().parent()
+                .find('input, textarea').clear().type(nome)
+        })
+
+        cy.generateValidCpf().then(cpf => {
+            sharedCpf = cpf;
+            cy.logToTerminal(`CPF gerado e salvo: ${cpf}`)
+            cy.contains('CPF').scrollIntoView().parent()
+                .find('input, textarea').clear().type(cpf)
+        })
+
+        cy.contains('Telefone').scrollIntoView().parent()
+            .find('input, textarea').clear().type('44444444444')
+
+        cy.generateRandomName().then(nome => {
+            cy.contains('Nome Social').scrollIntoView().parent()
+                .find('input, textarea').clear().type(nome)
+        })
+
+        cy.contains('Data de Nascimento ').scrollIntoView().parent()
+            .find('input').clear().type('2000-01-01')
+
+        cy.generateEmail().then(email => {
+            sharedEmail = email;
+            cy.logToTerminal(`Email gerado e salvo: ${email}`)
+            cy.contains('E-mail').scrollIntoView().parent()
+                .find('input, textarea').clear().type(email)
+        })
+
+        cy.contains('button', 'Próximo').scrollIntoView().click()
+
+        cy.contains('Digite seu CEP').scrollIntoView().parent()
+            .find('input, textarea').clear().type('87020-015')
+
+        cy.wait(1000)
+
+        cy.contains('Número').scrollIntoView().parent()
+            .find('input, textarea').clear().type('000')
+
+        cy.contains('Button', 'Próximo').scrollIntoView().click()
+
+        cy.get('button[role="combobox"]')
+            .contains('Selecione um curso')
+            .click();
+
+        cy.contains('ESPECIALIZAÇÃO EM ALFABETIZAÇÃO E LETRAMENTO')
             .should('be.visible')
             .click();
 
@@ -243,75 +238,6 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
 
         cy.contains('Button', 'Enviar').scrollIntoView().click()
 
-        cy.contains('button', 'Fechar', { timeout: 60000 })
-            .should('be.visible')
-            .and('be.enabled')
-    })
-
-    it('Deve retornar erro no cadastro - teste com mesmos dados de Nome, Email, CPF, Curso', () => {
-
-        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
-
-        cy.logToTerminal(`Usando Nome salvo: ${sharedName}`)
-        cy.contains('Nome Completo').scrollIntoView().parent()
-            .find('input, textarea').clear().type(sharedName)
-
-        cy.logToTerminal(`Usando Nome salvo: ${sharedCpf}`)
-        cy.contains('CPF').scrollIntoView().parent()
-            .find('input, textarea').clear().type(sharedCpf)
-
-        cy.contains('Telefone').scrollIntoView().parent()
-            .find('input, textarea').clear().type('44444444444')
-
-        cy.generateRandomName().then(nome => {
-            cy.contains('Nome Social').scrollIntoView().parent()
-                .find('input, textarea').clear().type(nome)
-        })
-
-        cy.contains('Data de Nascimento ').scrollIntoView().parent()
-            .find('input').clear().type('2000-01-01')
-
-        cy.logToTerminal(`Usando Email salvo: ${sharedEmail}`)
-        cy.contains('E-mail').scrollIntoView().parent()
-            .find('input, textarea').clear().type(sharedEmail)
-
-        cy.contains('button', 'Próximo').scrollIntoView().click()
-
-        cy.contains('Digite seu CEP').scrollIntoView().parent()
-            .find('input, textarea').clear().type('87080-400')
-
-        cy.wait(6000)
-
-        cy.contains('Número').scrollIntoView().parent()
-            .find('input, textarea').clear().type('000')
-
-        cy.contains('Button', 'Próximo').scrollIntoView().click()
-
-        cy.get('button[role="combobox"]')
-            .contains('Selecione um curso')
-            .click();
-
-        cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-            .should('be.visible')
-            .click();
-
-        cy.selecionarOpcaoAleatoriaDireto('Estado')
-        cy.selecionarOpcaoAleatoriaDireto('Cidade')
-        cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
-        cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
-        cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
-
-        cy.contains('label', 'Pix').click()
-
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
-
-        cy.wait(6000)
-
-        cy.contains('Button', 'Enviar').scrollIntoView().click()
-
-        cy.wait(1500)
-
         cy.contains('button', 'Voltar', { timeout: 60000 })
             .should('be.visible')
             .and('be.enabled')
@@ -320,7 +246,7 @@ describe('[PROD] Valida LP Pós-Graduação', () => {
 
     it('Deve retornar erro ao passar step 1 para 2, sem preenchimento', () => {
 
-        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
+        cy.visit('https://matriculapos.fatecie.edu.br/inscription')
 
         cy.contains('button', 'Próximo').scrollIntoView().click()
 
